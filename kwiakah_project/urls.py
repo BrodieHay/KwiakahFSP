23ea8e93261f0d54d49f50d4b9aa3d4177737431
Brodie Hay <brodiehay2334@gmail.com>
2021-01-15 11:57:44 -0800

    license added, and paginatino added to book pages

--- kwiakah_project/urls.py
+++ kwiakah_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='homePage'),
+    path('', include('stewardshipProgram.urls')),
     path('admin/', admin.site.urls),
     path('stewardshipProgram/', include('stewardshipProgram.urls')),
 
