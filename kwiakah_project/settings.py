17297210def365eb514a042f32ff9a4c74ee6b30
Brodie Hay <brodiehay2334@gmail.com>
2021-01-15 17:11:15 -0800

    modifying static root in settings.py

--- kwiakah_project/settings.py
+++ kwiakah_project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '../stewardshipProgram/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
