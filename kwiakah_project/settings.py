dd25a56796e03ab21560b8591b9d7346ef2db224
Brodie Hay <brodiehay2334@gmail.com>
2021-01-15 17:24:49 -0800

    modifying static root in settings.py

--- kwiakah_project/settings.py
+++ kwiakah_project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 
@@ -129,5 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "../stewardshipProgram/static/",
+    BASE_DIR / "../stewardshipProgram/static/   ",
 ]
