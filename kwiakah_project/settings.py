d3e2e78ec5fb9a6fa12722066840d4181e9433c6
Brodie Hay <brodiehay2334@gmail.com>
2021-01-17 10:32:15 -0800

    adjusting static files path

--- kwiakah_project/settings.py
+++ kwiakah_project/settings.py
@@ -128,5 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "../stewardshipProgram/static/   ",
+    BASE_DIR / "stewardshipProgram/static/   ",
 ]
