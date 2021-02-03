af3db1d53974f6c8c9acf4bad6b988314788bc02
Brodie Hay <brodiehay2334@gmail.com>
2021-02-02 17:25:00 -0800

    Preparing to add language selection buttons to book pages

--- stewardshipProgram/views.py
+++ stewardshipProgram/views.py
@@ -14,29 +14,94 @@ eyeHigh2 = []
 skyHigh1 = []
 skyHigh2 = []
 supportingBiodiversity = []
+booklist = [
+    aHealthyForest,
+    dirtAndRoots1,
+    dirtAndRoots2,
+    forestFloor1,
+    forestFloor2,
+    kneeHigh1,
+    kneeHigh2,
+    eyeHigh1,
+    eyeHigh2,
+    skyHigh1,
+    skyHigh2,
+    supportingBiodiversity,
+]
 
-def home(request):
-    return render(request, 'stewardshipProgram/home.html')
-
-def primary(request):
-    aHealthyForest.clear()
+def main():
+    for list in booklist:
+        list.clear()
     for filename in os.listdir(settings.BASE_DIR / 'stewardshipProgram/static/stewardshipProgram/images/lostSpiritBook'):
         aHealthyForest.append(filename)
-    aHealthyForest.sort()
+    for list in booklist:
+            list.sort()
+
+def home(request):
     context = {
+        'title':"Home",
+    }
+    return render(request, 'stewardshipProgram/home.html', context)
+
+def primary(request):
+    main()
+    context = {
+        'title':"Primary",
         'aHealthyForest' : aHealthyForest,
+        'dirtAndRoots2' : dirtAndRoots2,
+        'forestFloor2' : forestFloor2,
+        'kneeHigh2' : kneeHigh2,
+        'eyeHigh2' : eyeHigh2,
+        'skyHigh2' : skyHigh2,
+        'supportingBiodiversity' : supportingBiodiversity,
+
     }
     return render(request, 'stewardshipProgram/primary.html', context)
 
 
 def preschool(request):
-    return render(request, 'stewardshipProgram/preschool.html')
+    main()
+    context = {
+        'title':"Preschool",
+        'aHealthyForest' : aHealthyForest,
+        'dirtAndRoots1' : dirtAndRoots1,
+        'forestFloor1' : forestFloor1,
+        'kneeHigh1' : kneeHigh1,
+        'eyeHigh1' : eyeHigh1,
+        'skyHigh1' : skyHigh1,
+        'dirtAndRoots2' : dirtAndRoots2,
+        'forestFloor2' : forestFloor2,
+        'kneeHigh2' : kneeHigh2,
+        'eyeHigh2' : eyeHigh2,
+        'skyHigh2' : skyHigh2,
+        'supportingBiodiversity' : supportingBiodiversity,
+
+    }
+    return render(request, 'stewardshipProgram/preschool.html', context)
 
 def toddler(request):
-    return render(request, 'stewardshipProgram/toddler.html')
+    main()
+    context = {
+        'title':"Toddlers",
+        'aHealthyForest' : aHealthyForest,
+        'dirtAndRoots1' : dirtAndRoots1,
+        'forestFloor1' : forestFloor1,
+        'kneeHigh1' : kneeHigh1,
+        'eyeHigh1' : eyeHigh1,
+        'skyHigh1' : skyHigh1,
+        'supportingBiodiversity' : supportingBiodiversity,
+    }
+    return render(request, 'stewardshipProgram/toddler.html', context)
 
 def kidsCorner(request):
-    return render(request, 'stewardshipProgram/kidsCorner.html')
+    context = {
+        'title':"Kid's Corner",
+
+    }
+    return render(request, 'stewardshipProgram/kidsCorner.html', context)
 
 def resources(request):
-    return render(request, 'stewardshipProgram/resources.html')
+    context = {
+        'title':"Resources",
+    }
+    return render(request, 'stewardshipProgram/resources.html', context)
